e73d8d68fbc55fc850e9dfd5a7298178f8527570
G0RD4LH4 <gordalha77@gmail.com>
2024-12-04 16:22:06 -0300

    feat: cardápio semanal

---
+++ app/(auth)/cardapio/page.tsx
@@ -0,0 +1,11 @@
+export default function Menu() {
+    return (
+        <div className="w-[900px] p-6 text-black rounded-lg">
+          <h1 className="text-xl font-bold mb-4">Cardápio Semanal</h1>
+            <img
+                src="http://i.kian.com.br/src/uploads/2024/12/CARDAPIO-0212.jpg"
+                alt="Cardapio"
+            />
+        </div>
+    )
+}
--- app/(auth)/navbar.tsx
+++ app/(auth)/navbar.tsx
@@ -40,6 +40,10 @@ export default function Navbar({ userName }: { userName: string }) {
             <Button variant={'link'} className={cn( pathname === '#' ? 'underline' : '' )} >Requisições</Button>
           </Link>
 
+          <Link href="/cardapio" className="text-gray-700 hover:text-gray-900">
+            <Button variant={'link'} className={cn( pathname === '/cardapio' ? 'underline' : '' )} >Cardápio</Button>
+          </Link>
+
           <Link href="/fotos" className="text-gray-700 hover:text-gray-900">
             <Button variant={'link'} className={cn( pathname === '/fotos' ? 'underline' : '' )} >Fotos</Button>
           </Link>
