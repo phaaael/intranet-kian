692a88ec6bd11ad404bb0a3cdba52c1024047336
G0RD4LH4 <gordalha77@gmail.com>
2024-12-05 16:16:01 -0300

    feat: pagina de início

--- app/(auth)/inicio/page.tsx
+++ app/(auth)/inicio/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-    return <h1></h1>
+import CarouselDashboard from "./carousel-dashboard";
+
+export default function CarouselInicio() {
+  return (
+    <div className="p-6 text-black rounded-lg">
+      <h1 className="text-xl font-bold mb-4">Anúncios</h1>
+      <CarouselDashboard />
+    </div>
+)
 }
