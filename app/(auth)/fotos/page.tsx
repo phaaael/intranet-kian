40dd25786d4b78d79f0bdca2a30f3f95e117209d
G0RD4LH4 <gordalha77@gmail.com>
2024-12-05 21:02:45 -0300

    fix: tamanho do dashboard

--- app/(auth)/fotos/page.tsx
+++ app/(auth)/fotos/page.tsx
@@ -2,7 +2,7 @@ import PhotoDashboard from "./photos-dashboard";
 
 export default function Photos() {
     return (
-        <div className="w-[700px] p-6 text-black rounded-lg">
+        <div className="p-6 text-black rounded-lg">
           <h1 className="text-xl font-bold mb-4">Dashboard de Fotos</h1>
             <PhotoDashboard />
         </div>
