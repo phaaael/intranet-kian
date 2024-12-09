18990d80bd711949137aba372f3be5ded7ee6f08
G0RD4LH4 <gordalha77@gmail.com>
2024-12-09 09:13:52 -0300

    fix: tamanho maximo do cardápio

--- app/(auth)/cardapio/page.tsx
+++ app/(auth)/cardapio/page.tsx
@@ -1,11 +1,12 @@
 export default function Menu() {
     return (
-        <div className="w-[900px] p-6 text-black rounded-lg">
+        <div className="w-full max-w-4xl p-6 text-black rounded-lg mx-auto">
           <h1 className="text-xl font-bold mb-4">Cardápio Semanal</h1>
-            <img
-                src="http://i.kian.com.br/src/uploads/2024/12/CARDAPIO-0212.jpg"
-                alt="Cardapio"
-            />
+          <img
+              src="http://i.kian.com.br/src/uploads/2024/12/CARDAPIO-0212.jpg"
+              alt="Cardápio"
+              className="w-full h-auto object-contain rounded-lg"
+          />
         </div>
     )
 }
