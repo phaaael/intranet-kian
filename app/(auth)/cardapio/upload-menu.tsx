5c7aa264a9029d0a7418d22b20b7d867e650f368
G0RD4LH4 <gordalha77@gmail.com>
2024-12-11 14:40:49 -0300

    fix: após enviar o novo cardápio, o anexo é removido

--- app/(auth)/cardapio/upload-menu.tsx
+++ app/(auth)/cardapio/upload-menu.tsx
@@ -2,10 +2,11 @@
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { useState } from "react"
+import { useState, useRef } from "react"
 
 export default function UploadMenu({ onUploadComplete }: { onUploadComplete: () => void }) {
     const [file, setFile] = useState<File | null>(null)
+    const fileInputRef = useRef<HTMLInputElement | null>(null)
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.files && event.target.files[0]) {
@@ -15,7 +16,7 @@ export default function UploadMenu({ onUploadComplete }: { onUploadComplete: () 
 
     const handleUpload = async () => {
         if (!file) return
-        
+
         const formData = new FormData()
         formData.append("file", file)
 
@@ -26,6 +27,10 @@ export default function UploadMenu({ onUploadComplete }: { onUploadComplete: () 
             })
 
             if (response.ok) {
+                setFile(null)
+                if (fileInputRef.current) {
+                    fileInputRef.current.value = ""
+                }
                 onUploadComplete()
             }
         } catch (error) {
@@ -34,11 +39,16 @@ export default function UploadMenu({ onUploadComplete }: { onUploadComplete: () 
     }
 
     return (
-
         <div className="flex justify-center items-center p-8">
             <div className="border-2 border-slate-200 p-4 gap-2">
                 <p className="text-sm font-bold mb-4">Atualização do Cardápio</p>
-                <Input type="file" id="file" onChange={handleFileChange} className="w-full cursor-pointer mb-2" />
+                <Input
+                    ref={fileInputRef}
+                    type="file"
+                    id="file"
+                    onChange={handleFileChange}
+                    className="w-full cursor-pointer mb-2"
+                />
                 <Button
                     onClick={handleUpload}
                     disabled={!file}
