7c33d6c01f1f1e7a77d714687e4cd3f107039080
G0RD4LH4 <gordalha77@gmail.com>
2025-02-20 14:41:46 -0300

    fix: ordem dos dados no dashboard de ramais

--- app/api/phones/route.ts
+++ app/api/phones/route.ts
@@ -3,21 +3,24 @@ import db from "@/lib/db"
 
 export async function GET() {
   try {
-    const phones = await db.rp_phones.findMany({
-      select: {
-        phone: true,
-        employee: true,
-        sector: true,
-        walk: true,
-      },
-    })
+    const phones = await db.$queryRaw`
+      SELECT phone, employee, sector, walk 
+      FROM rp_phones 
+      ORDER BY 
+          CASE 
+          WHEN walk = 'Apoio' THEN 1
+              WHEN walk = 'Terreo' THEN 2
+          WHEN walk = '1º' THEN 3
+              WHEN walk = '2º' THEN 4
+              WHEN walk = '3º' THEN 5
+              WHEN walk = '-' THEN 6
+              ELSE 7
+          END;
+    `
 
-    return NextResponse.json(phones);
+    return NextResponse.json(phones)
   } catch (error) {
     console.error("Erro ao buscar dados da tabela rp_phones:", error)
-    return NextResponse.json(
-      { message: "Erro ao buscar dados", success: false },
-      { status: 500 }
-    )
+    return NextResponse.json({ message: "Erro ao buscar dados", success: false }, { status: 500 })
   }
 }
